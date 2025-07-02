9603b4837f0c92832b27ab1fe490546412a7393a
k4khuram <k4khuram@hotmail.com>
2025-07-02 23:08:51 +0100

    added test method

--- src/example/example.controller.ts
+++ src/example/example.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { ExampleService } from './example.service';
 
 @Controller('example')
 export class ExampleController {
   constructor(private readonly exampleService: ExampleService) {}
+
+  @Get('test')
+  async test(@Body() data: any) {
+    return "test";
+  }
 
   @Post('async-task')
   async addAsyncTask(@Body() data: any) {
