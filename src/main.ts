643bbc46f940641c949d5129db1ee246e81c92eb
k4khuram <k4khuram@hotmail.com>
2025-07-03 22:36:16 +0100

    fixed port issue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
+//import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
@@ -14,33 +14,13 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
  
   app.enableCors({
-    origin: [
-      'http://localhost:5174',
-      'http://localhost:3000',
-      'http://localhost:5173',
-      'https://creditsecrets.com',
-      // Add other domains as needed
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD'],
-    allowedHeaders: [
-      'Origin',
-      'X-Requested-With',
-      'Content-Type', 
-      'Accept',
-      'Authorization',
-      'Cache-Control',
-      'Pragma'
-    ],
-    credentials: true, // Can be true when using specific origins
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    maxAge: 86400,
+    origin: '*', // Allow all origins
   });
   
-  const configService = app.get(ConfigService);
+  //const configService = app.get(ConfigService);
 
 
-  const port = configService.get<number>('port');
+  //const port = configService.get<number>('port');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -68,9 +48,11 @@ async function bootstrap() {
   // const queueService = app.get(QueueService);
   // queueService.startProcessing();
 
-  await app.listen(port);
+  // await app.listen(port);
+  
+  await app.listen(process.env.PORT ?? 4000);
 
-  logger.log(`Application is running on: http://localhost:${port}`);
+  //logger.log(`Application is running on: http://localhost:${port}`);
 }
 
 bootstrap().catch((error) => {
