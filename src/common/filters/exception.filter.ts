943377d4fe90ebaf35af32ab179728731dd0940f
k4khuram <k4khuram@hotmail.com>
2025-07-28 19:11:01 +0100

    deploying2

--- src/common/filters/exception.filter.ts
+++ src/common/filters/exception.filter.ts
@@ -2,7 +2,7 @@ import { ExceptionFilter, Catch, ArgumentsHost, HttpException, Injectable, Logge
 import { Response } from 'express';
 import { MongoError } from 'mongodb';
 import { JobService } from '../services/job.service'
-import { JobType } from 'src/common/dto'
+import { JobType } from '../dto/create-job.dto'
 
 @Injectable()
 @Catch()
--- src/common/services/job.service.ts
+++ src/common/services/job.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Job, JobDocument } from '../schemas/job.schema';
-import { JobStatus, JobType } from 'src/common/dto';
+import { JobStatus, JobType } from '../dto/create-job.dto';
 
 @Injectable()
 export class JobService {
